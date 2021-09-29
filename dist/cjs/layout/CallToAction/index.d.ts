b93db6cfdbdf3ad43c705a99df31cbcec6e29a04
Tomek Marciniak <tmarciniakm@gmail.com>
2021-09-29 12:33:00 +0200

    fix(cta): add container to CTA

--- dist/cjs/layout/CallToAction/index.d.ts
+++ dist/cjs/layout/CallToAction/index.d.ts
@@ -4,6 +4,7 @@ declare type CallToActionProps = {
     heading: string;
     description?: string;
     children: React.ReactNode;
+    maxWidth?: BoxProps['maxWidth'];
 } & BoxProps;
-declare const CallToAction: ({ heading, description, children, ...containerProps }: CallToActionProps) => JSX.Element;
+declare const CallToAction: ({ heading, description, children, maxWidth, ...containerProps }: CallToActionProps) => JSX.Element;
 export default CallToAction;
--- src/layout/CallToAction/index.tsx
+++ src/layout/CallToAction/index.tsx
@@ -1,27 +1,32 @@
 import React from 'react'
-import { Flex, BoxProps, Box, Heading, Text } from '@chakra-ui/react'
+import { Flex, BoxProps, Box, Heading, Text, Container } from '@chakra-ui/react'
 
 type CallToActionProps = {
   heading: string
   description?: string
   children: React.ReactNode
+  maxWidth?: BoxProps['maxWidth']
 } & BoxProps
 
-const CallToAction = ({ heading, description, children, ...containerProps }: CallToActionProps) => {
+const CallToAction = ({ heading, description, children, maxWidth, ...containerProps }: CallToActionProps) => {
   return (
-    <Flex justify="space-between" align="center" data-testid="protochakra.cta" {...containerProps}>
-      <Box>
-        <Heading size="lg" data-testid="protochakra.cta.heading">
-          {heading}
-        </Heading>
-        {description && (
-          <Text marginTop="1rem" data-testid="protochakra.cta.description">
-            {description}
-          </Text>
-        )}
-      </Box>
-      <Box data-testid="protochakra.cta.addons">{children}</Box>
-    </Flex>
+    <Box paddingY="2rem" data-testid="protochakra.cta" {...containerProps}>
+      <Container maxWidth={maxWidth || 'container.xl'}>
+        <Flex justify="space-between" align="center">
+          <Box>
+            <Heading size="lg" data-testid="protochakra.cta.heading">
+              {heading}
+            </Heading>
+            {description && (
+              <Text marginTop="1rem" data-testid="protochakra.cta.description">
+                {description}
+              </Text>
+            )}
+          </Box>
+          <Box data-testid="protochakra.cta.addons">{children}</Box>
+        </Flex>
+      </Container>
+    </Box>
   )
 }
 
